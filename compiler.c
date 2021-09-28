f45c1352e0cbcfd9e3b4c283ec44462176798319
Matt DiPaolo <matt@dldipaolo.com>
2021-09-27 20:49:41 -0400

    fdA

--- compiler.c
+++ compiler.c
@@ -10,37 +10,37 @@ int main(int argc, char* argv[])
     //     num_lines++;
     while (fscanf(file,"%s",lines[num_lines]))
         num_lines++;
-    printf("%d\n",num_lines);
-    for (int i=0;i<num_lines;i++)
-        printf("%s",lines[i]);
+    // printf("%d\n",num_lines);
+    // for (int i=0;i<num_lines;i++)
+    //     printf("%s",lines[i]);
     
     
     
-    char *parsedData[num_lines][3];
-    int lineSize[num_lines];
-    char *token;
-    for (int i=0;i<num_lines;i++){
-        token = strtok(lines[i]," ");
-        int tmp = 0;
-        while (token != NULL){
-            parsedData[i][tmp] = token;
-            //printf("%s",token);
-            token = strtok(NULL, " ");
-            tmp++;
-        }
-        lineSize[i] = tmp+1;
-        //printf("%d\n",lineSize[i]);
-    }
+    // char *parsedData[num_lines][3];
+    // int lineSize[num_lines];
+    // char *token;
+    // for (int i=0;i<num_lines;i++){
+    //     token = strtok(lines[i]," ");
+    //     int tmp = 0;
+    //     while (token != NULL){
+    //         parsedData[i][tmp] = token;
+    //         //printf("%s",token);
+    //         token = strtok(NULL, " ");
+    //         tmp++;
+    //     }
+    //     lineSize[i] = tmp+1;
+    //     //printf("%d\n",lineSize[i]);
+    // }
 
 
 
 
 
 
-    for (int i = 0;i<num_lines;i++){
-        for (int j=0;j<lineSize[i];j++){}
-            //printf("%s ",parsedData[i][j]);
-    }
+    // for (int i = 0;i<num_lines;i++){
+    //     for (int j=0;j<lineSize[i];j++){}
+    //         //printf("%s ",parsedData[i][j]);
+    // }
     fclose(file);
     return 0;
 }
