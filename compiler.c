3807284c2bd0ae794d0792b1726f535f9f68ff18
Matt DiPaolo <matt@dldipaolo.com>
2021-09-27 20:47:29 -0400

    fdA

--- compiler.c
+++ compiler.c
@@ -6,11 +6,13 @@ int main(int argc, char* argv[])
     FILE* file = fopen("mad360.txt", "r"); /* should check the result */
     char lines[100][256];
     int num_lines = 0;
-    while (fgets(lines[num_lines], sizeof(lines[num_lines]), file))
+    // while (fgets(lines[num_lines], sizeof(lines[num_lines]), file))
+    //     num_lines++;
+    while (fscanf(file,"%s",lines[num_lines]))
         num_lines++;
-    for (int i=0;i<num_lines;i++)
-        printf("%s",lines[i]);
-    printf("%d\n",num_lines);
+    // for (int i=0;i<num_lines;i++)
+    //     printf("%s",lines[i]);
+    // printf("%d\n",num_lines);
     
     
     char *parsedData[num_lines][3];
