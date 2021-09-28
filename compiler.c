27d8b909c0552e08a886ef1f39ddb9f7daa5cbe3
Matt DiPaolo <matt@dldipaolo.com>
2021-09-27 20:11:11 -0400

    parsing each line =

--- compiler.c
+++ compiler.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
     FILE* file = fopen("mad360.txt", "r"); /* should check the result */
-
-
-
     char lines[100][256];
+    int num_lines = 0;
+    while (fgets(lines[num_lines], sizeof(lines[num_lines]), file))
+        num_lines++;
 
-
-
-    int i = 0;
-    while (fgets(lines[i], sizeof(lines[i]), file)) {
-        i++;
+    char *tmp
+    for (int i=0;i<num_lines;i++){
+        tmp = strtok(lines[i]," ");
+        for (int j=0;j<3;j++){
+            printf("%s\n",tmp[j]);
+        }
     }
-    for (int j=0;j<i;j++){
-        printf("%s", lines[j]);
-    }
-    /* may check feof here to make a difference between eof and io failure -- network
-       timeout for instance */
 
     fclose(file);
 
@@ -28,3 +25,4 @@ int main(int argc, char* argv[])
 
 
 //https://stackoverflow.com/questions/9206091/going-through-a-text-file-line-by-line-in-c
+//
