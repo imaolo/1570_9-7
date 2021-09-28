fac9f7f223e89bc0b7c33d1c5300c7636f41a317
Matt DiPaolo <matt@dldipaolo.com>
2021-09-27 20:05:54 -0400

    dsafds

--- compiler.c
+++ compiler.c
@@ -3,15 +3,19 @@
 int main(int argc, char* argv[])
 {
     FILE* file = fopen("mad360.txt", "r"); /* should check the result */
-    if (file == NULL){
-        printf("uh oh\n");
-    }
-    char line[256];
 
-    while (fgets(line, sizeof(line), file)) {
-        /* note that fgets don't strip the terminating \n, checking its
-           presence would allow to handle lines longer that sizeof(line) */
-        printf("%s", line); 
+
+
+    char lines[100][256];
+
+
+
+    int i = 0;
+    while (fgets(lines[i], sizeof(lines[i]), file)) {
+        i++;
+    }
+    for (int j=0;j<i;j++){
+        printf("%s\n", lines[j])
     }
     /* may check feof here to make a difference between eof and io failure -- network
        timeout for instance */
