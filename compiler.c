2800be85271fee5c8d1ef3e3c3bc11925338958d
Matt DiPaolo <matt@dldipaolo.com>
2021-09-27 20:02:09 -0400

    fdsafdsa

--- compiler.c
+++ compiler.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 
-int main() {
-   FILE *fp;
-   fp = fopen("mad360.txt", "r");
+int main(int argc, char* argv[])
+{
+    FILE* file = fopen("mad360.txt", "r"); /* should check the result */
+    if (file == NULL){
+        printf("uh oh\n");
+    }
+    char line[256];
 
-   char line[256];
-   fscanf(line ,fp, "%s" );
-   printf("%s\n", line);
-   fclose(fp);
-   return 0;
+    while (fgets(line, sizeof(line), file)) {
+        /* note that fgets don't strip the terminating \n, checking its
+           presence would allow to handle lines longer that sizeof(line) */
+        printf("%s", line); 
+    }
+    /* may check feof here to make a difference between eof and io failure -- network
+       timeout for instance */
+
+    fclose(file);
+
+    return 0;
 }
+
+
+
+//https://stackoverflow.com/questions/9206091/going-through-a-text-file-line-by-line-in-c
